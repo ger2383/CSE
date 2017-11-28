11fc360c58eaf06da66b9301995d7847dbdef980
ger2383 <33761415+ger2383@users.noreply.github.com>
2017-11-28 09:29:02 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,34 @@
+print("Hello World")
+
+# Ger Yang
+
+a = 4
+b = 3
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+print()
+print("Try to figure out how this works")
+print(13 % 12)
+
+# the "%" sign is a modules. It finds the reminder.
+
+car_name = "Wiebe Mobile"
+car_type = "BMW"
+car_cylinders = 8
+car_mpg = 5000.9
+
+print("I have a car called %s. It's pretty nice." % car_name)
+print("I have a car called %s. It's a %s." % (car_name, car_type)) # watch the order
+
+# Here is where we get a little fancy
+print("What is your name?")
+name = input(">_ ")
+print("Hello %s." % name)
+
+age = input("How old are you?")
+print("%s?! Thats really old. You belong in a retirement home." % age)
