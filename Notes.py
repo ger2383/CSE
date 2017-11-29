aef47644b0ee67ae9fae4b0cbcf3d1162cd27988
ger2383 <33761415+ger2383@users.noreply.github.com>
2017-11-29 09:46:12 -0800

    Added Functions
    11/29/17

--- Notes.py
+++ Notes.py
@@ -1,36 +1,91 @@
-print("Hello World")
+# print("Hello World")
+#
+# # Ger Yang
+#
+# a = 4
+# b = 3
+#
+# print(3 + 5)
+# print(5 - 3)
+# print(3 * 5)
+# print(6 / 2)
+# print(3 ** 2)
+#
+# print()
+# print("Try to figure out how this works")
+# print(13 % 12)
+#
+# # the "%" sign is a modules. It finds the reminder.
+#
+# car_name = "Wiebe Mobile"
+# car_type = "BMW"
+# car_cylinders = 8
+# car_mpg = 5000.9
+#
+# print("I have a car called %s. It's pretty nice." % car_name)
+# print("I have a car called %s. It's a %s." % (car_name, car_type)) # watch the order
+#
+# # Here is where we get a little fancy
+# print("What is your name?")
+# name = input(">_ ")
+# print("Hello %s." % name)
+#
+# age = input("How old are you?")
+# print("%s?! Thats really old. You belong in a retirement home." % age)
 
-# Ger Yang
+# Functions
 
-a = 4
-b = 3
 
-print(3 + 5)
-print(5 - 3)
-print(3 * 5)
-print(6 / 2)
-print(3 ** 2)
+def print_hw():
+    print("Hello World.")
+    print("Enjoy the day.")
 
-print()
-print("Try to figure out how this works")
-print(13 % 12)
 
-# the "%" sign is a modules. It finds the reminder.
+print_hw()
 
-car_name = "Wiebe Mobile"
-car_type = "BMW"
-car_cylinders = 8
-car_mpg = 5000.9
 
-print("I have a car called %s. It's pretty nice." % car_name)
-print("I have a car called %s. It's a %s." % (car_name, car_type)) # watch the order
+def say_hi(name): # Name is a "parameter"
+    print("Hello %s" % name)
+    print("Coding is great!")
 
-# Here is where we get a little fancy
-print("What is your name?")
-name = input(">_ ")
-print("Hello %s." % name)
 
-age = input("How old are you?")
-print("%s?! Thats really old. You belong in a retirement home." % age)
+say_hi("John")
 
-# This is your life saver
+
+def print_age(name, age):
+    print("%s is %d years old" % (name, age))
+    age += 1  # age = age + 1
+    print("next year, %s will be %d years old"% (name, age))
+
+
+print_age("Ger", 14)
+
+
+def algebra_hw(x):
+    return x**3 + 4*x**2 + 7 * x -4
+
+
+print(algebra_hw(3))
+print(algebra_hw(4))
+print(algebra_hw(5))
+print(algebra_hw(6))
+print(algebra_hw(7))
+
+
+# if statements
+
+
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:  # else if
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    elif percentage >= 50:
+        return "F"
+
+
+grade_calc(92)
