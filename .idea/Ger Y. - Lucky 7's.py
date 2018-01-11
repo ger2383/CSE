01ffc9a8edc5e8196e9ef0e1b956e567f8acd221
ger2383 <33761415+ger2383@users.noreply.github.com>
2018-01-11 09:27:06 -0800

    1/10/18
    changes some codes

--- .idea/Ger Y. - Lucky 7's.py
+++ .idea/Ger Y. - Lucky 7's.py
@@ -1,7 +1,10 @@
 import random
 dice1 = (random.randint(1, 6))
 dice2 = (random.randint(1, 6))
-print(dice1)
-print(dice2)
+money_left = 15
 
+while money_left != 0:
+    print(dice1)
+    print(dice2)
+    
 
