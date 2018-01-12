16164e05f3ea84ce69237e4840e58ca0010e9522
ger2383 <33761415+ger2383@users.noreply.github.com>
2018-01-12 10:26:47 -0800

    1/12/18
    added codes

--- .idea/Ger Y. - Lucky 7's.py
+++ .idea/Ger Y. - Lucky 7's.py
@@ -4,6 +4,11 @@ money_left = 15
 while money_left > 0:
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
-    total = dice1 + dice2
+    sum = dice1 + dice2
+
+    if sum == 7:
+        print("You Won")
+        money_left += 5
+
 
 
