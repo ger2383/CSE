a8098aca24a018ad1be5a0e8b864e73f1a372726
ger2383 <33761415+ger2383@users.noreply.github.com>
2018-03-21 09:44:42 -0700

    Add more

--- Ger Yang - Items.py
+++ Ger Yang - Items.py
@@ -13,6 +13,14 @@ class Item(object):
     def weight(self):
         print("The % is about 12 pound" % self.weight)
 
+    def pick_up(self):
+        print("You pick up the item")
+
+    def put_on(self):
+        print("Attachment on")
+
+    def heal(self):
+        print("You just healed")
 
 class Weapon(Item):
     def __init__(self, name, value, weight):
@@ -26,62 +34,100 @@ class Pistol(Weapon):
     def __int__(self, name, value, weight):
         super(Pistol, self).__int__(name, value, weight)
 
+    def fire(self):
+        print("You fire the gun.")
+
 
 class Shotgun(Weapon):
     def __init__(self, name, value, weight):
         super(Shotgun, self).__init__(name, value, weight)
 
+    def fire(self):
+        print("You fire the gun.")
 
-class knives(Weapon):
+
+class knive(Weapon):
     def __init__(self, name, value, weight):
-        super(knives, self).__init__(name, value, weight)
+        super(knive, self).__init__(name, value, weight)
+
+    def Stab(self):
+        print("You used the knive.")
+
 
 class M_16(Weapon):
     def __init__(self, name, value, weight):
         super(M_16, self).__init__(name, value, weight)
+
+    def fire(self):
+        print("You fire the gun.")
 
 
 class Attachments(Item):
     def __init__(self, name, value, weight):
         super(Attachments, self).__init__(name, value, weight)
 
+    def put_on(self):
+        Item.put_on(self)
+
 
 class Pistol_Silencer(Attachments):
     def __init__(self, name, value, weight):
         super(Pistol_Silencer, self).__init__(name, value, weight)
+
+    def put_on(self):
+        Item.put_on(self.name)
 
 
 class Scope(Attachments):
     def __init__(self, name, value, weight):
         super(Scope, self).__init__(name, value, weight)
 
+    def put_on(self):
+        Item.put_on(self.name)
+
 
 class Armor(Item):
     def __init__(self, name, value, weight):
         super(Armor, self).__init__(name, value, weight)
+
+    def put_on(self):
+        Item.put_on(self.name)
 
 
 class Heavy_Armor(Armor):
     def __init__(self, name, value, weight):
         super(Heavy_Armor, self).__init__(name, value, weight)
 
+    def put_on(self):
+        Item.put_on(self.name)
+
 
 class Light_Armor(Armor):
     def __init__(self, name, value, weight):
         super(Light_Armor, self).__init__(name, value, weight)
+
+    def put_on(self):
+        Item.put_on(self.name)
 
 
 class Healing_Item(Item):
     def __init__(self, name, value, weight):
         super(Healing_Item, self).__init__(name, value, weight)
 
+    def heal(self):
+        Item.heal(self)
+
 
 class Bandage(Healing_Item):
     def __init__(self, name, value, weight):
         super(Bandage, self).__init__(name, value, weight)
 
+    def heal(self):
+        Item.heal(self)
 
-class Medic_Pack(Healing Item):
+class Medic_Pack(Healing_Item):
     def __init__(self, name, value, weight):
         super(Medic_Pack, self).__init__(name, value, weight)
 
+    def heal(self):
+        Item.heal(self)
